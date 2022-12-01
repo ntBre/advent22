064fdce1280ad8778d4693efc787b5ef5b20ea21
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-01 06:33:01 -0500

    add place for pasting sample in template

--- template.rs
+++ template.rs
@@ -1,8 +1,12 @@
+const SAMPLE: &str = "
+";
+
 fn load_input() -> String {
     std::fs::read_to_string("input").unwrap()
 }
 
 fn main() {
     let input = load_input();
+    println!("{}", SAMPLE);
     println!("{}", input);
 }

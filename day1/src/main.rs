38663104d5241795a3a928820bcf4b1af80309cd
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-01 07:47:51 -0500

    tidy up day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,14 +1,5 @@
 use advent22::*;
 
-fn main() {
-    let input = load_input();
-    let mut elves = part1(&input);
-    elves.sort();
-    elves.reverse();
-    dbg!(elves[..3].iter().sum::<usize>());
-    // println!("{}", input);
-}
-
 fn part1(inp: &str) -> Vec<usize> {
     let mut elves = Vec::new();
     let mut sum = 0;
@@ -21,6 +12,14 @@ fn part1(inp: &str) -> Vec<usize> {
         }
     }
     elves.push(sum);
-    println!("max = {}", elves.iter().max().unwrap());
     elves
+}
+
+fn main() {
+    let input = load_input();
+    let mut elves = part1(&input);
+    elves.sort();
+    elves.reverse();
+    println!("part1 = {}", elves.iter().max().unwrap());
+    println!("part2 = {}", elves[..3].iter().sum::<usize>());
 }

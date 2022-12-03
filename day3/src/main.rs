5790045c0460660c8906fd4ab7b94302f8dafdc7
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-03 11:06:45 -0500

    iterators!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -12,18 +12,20 @@ fn priority(b: u8) -> usize {
 
 fn main() {
     let s = load_input();
-    let mut sets: Vec<HashSet<u8, RandomState>> = Vec::new();
-    for line in s.lines() {
-        sets.push(HashSet::from_iter(line.bytes()));
-    }
 
-    let mut tot = 0;
-    for s in sets.chunks(3) {
-        let (a, b, c) = (&s[0], &s[1], &s[2]);
-        let ab: HashSet<u8, RandomState> =
-            HashSet::from_iter(a.intersection(b).cloned());
-        let abc = ab.intersection(c).next().unwrap();
-        tot += priority(*abc);
-    }
+    let tot: usize = s
+        .lines()
+        .map(|line| HashSet::from_iter(line.bytes()))
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|s| {
+            let [a, b, c] = s else { unreachable!() };
+            let ab: HashSet<u8, RandomState> =
+                HashSet::from_iter(a.intersection(b).cloned());
+            priority(*ab.intersection(c).next().unwrap())
+        })
+        .sum();
+
+    assert_eq!(tot, 2838);
     println!("part 2 = {tot}");
 }

6b35884a7569481fc3d709f8efbe3819c4cf75e4
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-08 07:43:44 -0500

    bring back part1 and get it down to a "single" if

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,16 +1,29 @@
 use advent22::*;
 
-fn main() {
-    let s = load_input();
-    let grid: Vec<_> = s
-        .lines()
-        .map(|line| {
-            line.chars()
-                .flat_map(|c| char::to_digit(c, 10))
-                .collect::<Vec<_>>()
-        })
-        .collect();
+fn part1(grid: &Vec<Vec<u32>>) -> usize {
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut tot = 0;
+    for (i, row) in grid.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if i == 0
+                || i == rows - 1
+                || j == 0
+                || j == cols - 1
+                || grid.iter().take(i).all(|x| *col > x[j])
+                || grid.iter().rev().take(rows - i - 1).all(|x| *col > x[j])
+                || grid[i].iter().take(j).all(|x| col > x)
+                || grid[i].iter().rev().take(cols - j - 1).all(|x| col > x)
+            {
+                tot += 1;
+                continue;
+            }
+        }
+    }
+    tot
+}
 
+fn part2(grid: Vec<Vec<u32>>) -> usize {
     let rows = grid.len();
     let cols = grid[0].len();
     let mut scores = vec![vec![1; cols]; rows];
@@ -61,12 +74,28 @@ fn main() {
             scores[i][j] *= count;
         }
     }
-    println!(
-        "{}",
-        scores
-            .iter()
-            .map(|row| row.iter().max().unwrap())
-            .max()
-            .unwrap()
-    );
+    *scores
+        .iter()
+        .map(|row| row.iter().max().unwrap())
+        .max()
+        .unwrap()
+}
+
+fn main() {
+    let s = load_input();
+    let grid: Vec<_> = s
+        .lines()
+        .map(|line| {
+            line.chars()
+                .flat_map(|c| char::to_digit(c, 10))
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let p1 = part1(&grid);
+    let p2 = part2(grid);
+    assert_eq!(p1, 1843);
+    assert_eq!(p2, 180000);
+    println!("{}", p1);
+    println!("{}", p2);
 }

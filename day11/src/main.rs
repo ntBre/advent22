ad73eb11dbdccecbf091919c9d503233aadc58a6
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-11 13:46:56 -0500

    replicate part 1 while separating prod and sum

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,16 +1,102 @@
 #![allow(unused)]
+use std::{
+    default,
+    fmt::Display,
+    num::ParseIntError,
+    ops::{Add, DivAssign, Mul, MulAssign, Rem},
+    str::FromStr,
+};
+
 use advent22::*;
 
+#[derive(Clone, Default)]
+struct Worry {
+    prod: i128,
+    sum: i128,
+}
+
+impl DivAssign<i128> for Worry {
+    fn div_assign(&mut self, rhs: i128) {
+        self.prod = (self.prod + self.sum) / rhs;
+        self.sum = 0;
+    }
+}
+
+impl Rem<i128> for Worry {
+    type Output = i128;
+
+    fn rem(self, rhs: i128) -> Self::Output {
+        (self.prod + self.sum) % rhs
+    }
+}
+
+impl Add<i128> for Worry {
+    type Output = Worry;
+
+    fn add(mut self, rhs: i128) -> Self::Output {
+        self.sum += rhs;
+        self
+    }
+}
+
+impl Mul<i128> for Worry {
+    type Output = Worry;
+
+    fn mul(mut self, rhs: i128) -> Self::Output {
+        self.prod *= rhs;
+        self
+    }
+}
+
+impl From<Worry> for i128 {
+    fn from(value: Worry) -> Self {
+        value.prod + value.sum
+    }
+}
+
+impl FromStr for Worry {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let val = s.parse()?;
+        Ok(Worry {
+            prod: val,
+            ..Default::default()
+        })
+    }
+}
+
+impl Display for Worry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.prod + self.sum)
+    }
+}
+
 struct Monkey {
-    items: Vec<usize>,
+    items: Vec<Worry>,
     /// takes the old worry level and returns the new
-    op: Box<dyn Fn(usize) -> usize>,
+    op: Box<dyn Fn(Worry) -> Worry>,
     /// performs the test and returns the monkey to throw to
-    test: Box<dyn Fn(usize) -> usize>,
+    test: Box<dyn Fn(Worry) -> usize>,
+}
+
+struct Monkeys<'a>(&'a Vec<Monkey>);
+
+impl Display for Monkeys<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, monkey) in self.0.iter().enumerate() {
+            write!(f, "Monkey {i}: ")?;
+            for item in &monkey.items {
+                write!(f, "{item}, ")?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 fn main() {
-    let s = load_input();
+    let s = load_sample();
     let mut monkeys = Vec::new();
     let mut cur_items = Vec::new();
     let mut cur_op = String::new();
@@ -58,10 +144,13 @@ fn main() {
             let cur_op = std::mem::take(&mut cur_op);
             monkeys.push(Monkey {
                 items: std::mem::take(&mut cur_items),
-                op: Box::new(move |old| match cur_op.as_str() {
-                    "*" => old * cur_const.unwrap_or(old),
-                    "+" => old + cur_const.unwrap_or(old),
-                    _ => panic!("unknown op {cur_op}"),
+                op: Box::new(move |old| {
+                    let c = cur_const.unwrap_or_else(|| old.clone().into());
+                    match cur_op.as_str() {
+                        "*" => old * c,
+                        "+" => old + c,
+                        _ => panic!("unknown op {cur_op}"),
+                    }
                 }),
                 test: Box::new(move |worry| {
                     if worry % cur_div == 0 {
@@ -79,16 +168,18 @@ fn main() {
         for m in 0..monkeys.len() {
             let items = std::mem::take(&mut monkeys[m].items);
             for mut worry in items {
-		inspections[m] += 1;
+                inspections[m] += 1;
                 worry = (monkeys[m].op)(worry);
                 worry /= 3;
-                let targ = (monkeys[m].test)(worry);
+                let targ = (monkeys[m].test)(worry.clone());
                 monkeys[targ].items.push(worry);
             }
         }
+        println!("round {}:\n{}", round + 1, Monkeys(&monkeys));
     }
     inspections.sort();
     inspections.reverse();
+    dbg!(&inspections);
     let p1 = inspections[0] * inspections[1];
     println!("{p1}");
 }

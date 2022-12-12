33ccdfe010911b359bb365f19c829ccfc4a9c183
Brent Westbrook <brentrwestbrook@gmail.com>
2022-12-12 12:14:35 -0500

    pass day12 part1 sample, too slow for input

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,50 @@
 use advent22::*;
 use std::{collections::HashMap, ops::Index};
 
+#[derive(Debug)]
+struct Tree<T> {
+    nodes: Vec<Node<T>>,
+}
+
+impl<T> Tree<T> {
+    fn new(data: T) -> Self {
+        Self {
+            nodes: vec![Node {
+                parent: None,
+                children: Vec::new(),
+                data,
+            }],
+        }
+    }
+
+    /// push a [Node] containing `data` into the tree and return its index in the tree
+    fn push(&mut self, idx: usize, data: T) -> usize {
+        let l = self.nodes.len();
+        self.nodes.push(Node {
+            parent: Some(idx),
+            children: Vec::new(),
+            data,
+        });
+        self.nodes[idx].children.push(l);
+        l
+    }
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    /// index into containing tree. None for root of tree
+    parent: Option<usize>,
+    /// also indices into the containing tree
+    children: Vec<usize>,
+    data: T,
+}
+
+impl<T> Node<T> {
+    fn inner(&self) -> &T {
+        &self.data
+    }
+}
+
 fn to_height(c: char) -> usize {
     let map: HashMap<char, usize> = ('a'..='z').zip(0..26).collect();
     map[&c]
@@ -47,58 +91,82 @@ fn load_grid(s: String) -> (Grid, Point, Point) {
     (Grid(grid), start, end)
 }
 
-fn generate_moves(pos: Point, grid: &Grid) -> Vec<Point> {
+fn generate_moves(
+    tree: &Tree<Point>,
+    node: &Node<Point>,
+    grid: &Grid,
+) -> Vec<Point> {
+    let pos = node.inner();
     let (x, y) = pos;
-    let h = grid[pos];
+    let h = grid[*pos];
     let mut ret = Vec::new();
-    if x > 0 {
-        let h2 = grid[(x - 1, y)];
+    if *x > 0 {
+        let h2 = grid[(*x - 1, *y)];
         if h2 <= h + 1 {
-            ret.push((x - 1, y));
+            ret.push((*x - 1, *y));
         }
     }
-    if y > 0 {
-        let h2 = grid[(x, y - 1)];
+    if *y > 0 {
+        let h2 = grid[(*x, *y - 1)];
         if h2 <= h + 1 {
-            ret.push((x, y - 1));
+            ret.push((*x, *y - 1));
         }
     }
 
     let (mx, my) = grid.shape();
-    if x < mx - 1 {
-        let h2 = grid[(x + 1, y)];
+    if *x < mx - 1 {
+        let h2 = grid[(*x + 1, *y)];
         if h2 <= h + 1 {
-            ret.push((x + 1, y));
+            ret.push((*x + 1, *y));
         }
     }
-    if y < my - 1 {
-        let h2 = grid[(x, y + 1)];
+    if *y < my - 1 {
+        let h2 = grid[(*x, *y + 1)];
         if h2 <= h + 1 {
-            ret.push((x, y + 1));
+            ret.push((*x, *y + 1));
         }
     }
-    ret
+    ret.into_iter()
+        .filter(|p| {
+            let mut cur = node.parent;
+            while let Some(parent) = cur {
+                if p == tree.nodes[parent].inner() {
+                    return false;
+                }
+                cur = tree.nodes[parent].parent;
+            }
+            true
+        })
+        .collect()
 }
 
 fn main() {
     let s = load_sample();
     let (grid, start, end) = load_grid(s);
+    let mut tree = Tree::new(start);
     // build tree of legal moves at every position
-    let mut moves = generate_moves(start, &grid);
+    let mut to_visit = vec![0];
     let mut round = 1;
     'outer: loop {
-        let mut new_moves = Vec::new();
-        for mov in std::mem::take(&mut moves) {
-            if mov == end {
-                break 'outer;
+        let now = std::time::Instant::now();
+        let tv = std::mem::take(&mut to_visit);
+        for cur in tv {
+            let moves = generate_moves(&tree, &tree.nodes[cur], &grid);
+            for mov in moves {
+                if mov == end {
+                    break 'outer;
+                }
+                to_visit.push(tree.push(cur, mov));
             }
-            new_moves.extend(generate_moves(mov, &grid));
         }
-        moves = new_moves;
+        println!(
+            "finished round {round} after {:.1}",
+            now.elapsed().as_millis() as f64 / 1000.0
+        );
         round += 1;
-        dbg!(round, moves.len());
     }
     dbg!(round);
+    // dbg!(round);
     // find paths through tree terminating at end
     // choose the shortest one
 }
